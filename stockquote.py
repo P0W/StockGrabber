c14f9a8e8e5a20701b6f6a8ee072f60afd4d9861
P0W <powprashant@gmail.com>
2016-10-28 16:36:22 +0530

    Add files via upload
    Initial commit

---
+++ stockquote.py
@@ -0,0 +1,88 @@
+from urllib.request import *
+import json
+import time
+import winsound
+import copy
+
+class GoogleFinanceStockGrabber:
+    def __init__(self):
+        proxy = ProxyHandler({'http': 'http://approxy.rockwellcollins.com:9090'})
+        opener = build_opener(proxy)
+        install_opener(opener)
+        self.prefix = "http://finance.google.com/finance/info?client=ig&q="
+
+    def get(self,symbol):
+        url = self.prefix+"%s"%(symbol)
+        u = urlopen(url)
+        content = u.read().decode('utf-8')
+        
+        obj = json.loads(content[3:])
+        return obj
+
+    def stockPrinter( self, symb ):
+        print ( '\n%10s | %-6s : %10s (%8s %% ) < %s >' % (symb['t'], symb['e'], symb['l'], symb['cp'], symb['lt'] ) )
+          
+class StockChangeDetectorRunner(  GoogleFinanceStockGrabber ):
+    def __init__(self, AllStocks):
+        super(StockChangeDetectorRunner, self).__init__()
+        self.AllStocks = copy.deepcopy(AllStocks)
+        for _ in self.AllStocks:
+            self.AllStocks[_] = 0.0
+            
+    def run(self):
+        quote = self.get(','.join( self.AllStocks.keys() ) )
+        for symb in quote:
+            stockKey = '%s:%s' % ( symb['e'], symb['t'] )
+            if symb['cp'] != self.AllStocks[ stockKey ]:
+                self.AllStocks[ stockKey ] = symb['cp']
+                self.stockPrinter( symb )
+                
+class BuyCallRunner( GoogleFinanceStockGrabber ):
+    def __init__(self, AllStocks, Target ):
+        super( BuyCallRunner, self).__init__()
+        self.AllStocks = copy.deepcopy(AllStocks)
+        self.target = Target
+  
+    def run(self):
+        quote = self.get(','.join( self.AllStocks.keys() ) )
+        total = 0.0
+        for symb in quote:
+            stockKey = '%s:%s' % ( symb['e'], symb['t'] )
+            total += float(symb['l'].replace(',','')) * self.AllStocks[ stockKey ]
+        
+        if total < self.target:
+            print ( 'Total = %.2f' % total )                
+            winsound.Beep( 2500, 500 )
+        
+    
+if __name__ == "__main__":
+    
+##    AllStocks = { 'NSE:PFS'    : 0.0,
+##                  'NSE:STAR'   : 0.0,
+##                  'NSE:INFY'   : 0.0,
+##                  'NSE:SBIN'   : 0.0,
+##                  'NSE:CANBK'  : 0.0,
+##                  'NSE:BERGER' : 0.0,
+##                  'NSE:DABUR'  : 0.0,
+##                  'NSE:TATAMOTORS':0.0,
+##                     }
+
+    AllStocks = {
+                  'BOM:509480' : 25.0, ## Berger Paints
+                  'BOM:500233' : 15.0, ##'NSE:KAJARIACER'
+                  'BOM:532540' : 6.0,  ##'NSE:TCS'         : 6.0,
+                  'BOM:532500' : 2.0,  ##'NSE:MARUTI'      : 2.0,
+                  'BOM:500096' : 30.0  ##'NSE:DABUR'       : 30.0,
+                  
+                }
+    
+    changeDetector = StockChangeDetectorRunner(AllStocks)
+    buycall = BuyCallRunner( AllStocks, 51000.0 )
+    
+    while True:
+        try:
+            changeDetector.run()
+            buycall.run()
+        except:
+            print ('Failed to Fetch...' )
+        time.sleep(5)
